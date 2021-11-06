321be23f8e72b044023229c7256cc954d1e19053
echoaj <aljoslin13@yahoo.com>
2021-11-06 15:58:24 -0700

    stuff changed

--- Vunsh_Mehta/FinanceLife/script.js
+++ Vunsh_Mehta/FinanceLife/script.js
@@ -17,39 +17,59 @@ start = {
 }
 
 
-
-
-// Level 1
-function jobPicked() {
-    cash += 40000;
-    money.innerText = "Money: $" + cash;
-    time.innerText = "Time: 1 year";
-    lifeStatus.innerText = "You have a new job \n press click to continue."
-    button1.style.display = "none";
-    button2.style.display = "none";
+// update variables
+function updateState(){
+    time += list[0];
+    cash += list[1];
+    buttonName1 = list[2];
+    buttonName2 = list[3];
 }
 
-let button1 = document.getElementById("job");
-let button2 = document.getElementById("college");
-let lifeStatus = document.getElementById("status")
-let money = document.getElementById("money");
-let time = document.getElementById("time");
-let cash = 0
+// update html
+function updateHTML(){
+    timeEL.innerText = `Time: ${time} years`;
+    moneyEL.innerText = `Money: ${cash}`;
+    button1EL.innerText = buttonName1;
+    button2EL.innerText = buttonName2;
+}
+
+function nextLevel(i){
+    let choice = list[i];
+    list = start[choice];
+}
+
+function button(option) {
+    nextLevel(option);
+    updateState();
+    updateHTML();
+}
+
+let button1EL = document.getElementById("job");
+let button2EL = document.getElementById("college");
+let statusEL = document.getElementById("status")
+let moneyEL = document.getElementById("money");
+let timeEL = document.getElementById("time");
+
+let cash = 0;
+let time = 0;
+let buttonName1 = "";
+let buttonName2 = "";
+let list = start["life"];
 
 
-document.onclick = function (event) {
-    // Compensate for IE<9's non-standard event model
-    //
-    if (event === undefined) event = window.event;
-    var target = 'target' in event ? event.target : event.srcElement;
+button1EL.addEventListener("click", button.bind(this, 2));
+button2EL.addEventListener("click", button.bind(this, 3));
 
-    alert('clicked on ' + target.tagName);
-};
+// document.onclick = function (event) {
+//     // Compensate for IE<9's non-standard event model
+//     //
+//     if (event === undefined) event = window.event;
+//     var target = 'target' in event ? event.target : event.srcElement;
+
+//     alert('clicked on ' + target.tagName);
+// };
 
 // jobButton.onclick = jobPicked;
-button1.addEventListener("click", jobPicked, false);
-
-
 
 
 
@@ -76,6 +96,7 @@ button1.addEventListener("click", jobPicked, false);
 
 
 /*
+
 var money = 0;
 var time = 0;
 var list = start["life"];
@@ -120,14 +141,5 @@ let cashEL = 0;
 
 button1EL.addEventListener("click", button.bind(this, 2), false);
 button2EL.addEventListener("click", button.bind(this, 3), false);
+
 */
-
-
-
-
-
-
-
-
-
-
