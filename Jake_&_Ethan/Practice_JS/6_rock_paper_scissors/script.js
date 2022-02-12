8d6251fe83bcc78b236e197456550c5dd0f63f4d
echoaj <aljoslin13@yahoo.com>
2022-02-12 13:56:22 -0800

    jake ethan lesson

---
+++ Jake_&_Ethan/Practice_JS/6_rock_paper_scissors/script.js
@@ -0,0 +1,25 @@
+
+
+const rock = document.querySelector("img:nth-of-type(1)");
+const paper = document.querySelector("img:nth-of-type(2)");
+const scissors = document.querySelector("img:nth-of-type(3)");
+const plyrChoice = document.querySelector(".plr");
+const compChoice = document.querySelector(".cmp");
+const winStatus = document.querySelector(".win");
+
+
+
+rock.addEventListener('click', () => {
+    plyrChoice.insertAdjacentHTML("beforeend", '<img src="rock.png" alt="rock" width="50px">')
+});
+
+
+paper.addEventListener('click', () => {
+    console.log("Hello paper");
+});
+
+scissors.addEventListener('click', () => {
+    console.log("Hello scissors");
+});
+
+
