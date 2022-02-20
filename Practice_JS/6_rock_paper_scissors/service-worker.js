d84a2323fa4b563be105badef748777efde8e949
echoaj <aljoslin13@yahoo.com>
2022-02-19 22:40:26 -0800

    rps update

--- Practice_JS/6_rock_paper_scissors/service-worker.js
+++ Practice_JS/6_rock_paper_scissors/service-worker.js
@@ -1,7 +1,23 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
+const { registerRoute } = workbox.routing;
+const { CacheFirst } = workbox.strategies;
+const { CacheableResponse } = workbox.cacheableResponse;
+const { precacheAndRoute } = workbox.precaching;
+
+
+// precacheAndRoute([
+//   { url: '/index.html', revision: null },
+//   { url: '/style.css', revision: null },
+//   { url: '/paper.jpg', revision: null },
+//   // ... other entries ...
+// ]);
+
+
 // This will work!
-workbox.routing.registerRoute(
-  ({request}) => request.destination === 'image',
-  new workbox.strategies.CacheFirst()
+registerRoute(
+  ({ request }) => request.destination === 'image',
+  new CacheFirst()
 );
+
+
