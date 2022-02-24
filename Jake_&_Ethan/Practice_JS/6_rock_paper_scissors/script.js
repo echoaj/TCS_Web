3bbba592fe83cea63e4d2d6884b22618d01194c2
echoaj <aljoslin13@yahoo.com>
2022-02-23 19:01:21 -0800

    deleted spaces

--- Jake_&_Ethan/Practice_JS/6_rock_paper_scissors/script.js
+++ Jake_&_Ethan/Practice_JS/6_rock_paper_scissors/script.js
@@ -1,12 +1,10 @@
 
-
 const rock = document.querySelector("img:nth-of-type(1)");
 const paper = document.querySelector("img:nth-of-type(2)");
 const scissors = document.querySelector("img:nth-of-type(3)");
 const plyrChoice = document.querySelector(".plr");
 const compChoice = document.querySelector(".cmp");
 const winStatus = document.querySelector(".win");
-
 
 
 rock.addEventListener('click', () => {
