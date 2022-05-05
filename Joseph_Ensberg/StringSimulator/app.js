f110eeb13c2c4cd714a199e6dca2b878f211892e
echoaj <aljoslin13@yahoo.com>
2022-05-04 18:31:00 -0700

    new joseph lesson

--- Joseph_Ensberg/StringSimulator/app.js
+++ Joseph_Ensberg/StringSimulator/app.js
@@ -1,131 +1,144 @@
 //Define Variables
-let Playing = false;
-let Pressed_Keys = [];
-
+let playing = false;
+let pressedKeys = [];
 
 //Canvas
+const CANVAS_WIDTH = 1400;
+const CANVAS_HEIGHT = 700;
 let gameWindow = document.getElementById("gameWindow");
-let item = gameWindow.getContext("2d");
-let Canvas_Width = 1400;
-let Canvas_Height = 700;
+let canvas = gameWindow.getContext("2d");
 
 //Player
-let Player_Size = 10;
-let Player_X = (Canvas_Width / 2);
-let Player_Y = (Canvas_Height / 2);
-let Player_Color = "blue";
-let Player_Length = 10;
-let Player_Segments = [{ X: Player_X, Y: Player_Y }];
-for (let i = 0; i < (Player_Length - 1) * Player_Size; i++) {
-    Player_Segments.push({ X: Player_X - i, Y: Player_Y });
+let playerSize = 10;
+let playerX = (CANVAS_WIDTH / 2);
+let playerY = (CANVAS_HEIGHT / 2);
+let playerColor = "blue";
+let playerLength = 5;
+let playerSegments = [{ "x": playerX, "y": playerY }]
+for (let i = 0; i < (playerLength - 1) * playerSize; i++) {
+    playerSegments.push({ "x": playerX - i, "y": playerY })
 };
 
-
-//Barriers
-/*let barrier = gameWindow.getContext("2d");
+//Obstacles
 let currentObstacles = [];
-let obstacles = [{ "name": barrier, "type": "rect", "width": Canvas_Height / 20, "height": Canvas_Height / 20, "x": Canvas_Height / 20, "y": 0, "color":"gray" }];
-//Functions
+let barrierChance = 1; //Posible chances barrier spawns each tick
+
+
 function GetRandom(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 };
-//*/
 
-// Makes either rectangle or cicle
-function MakeItem(X, Y, Width, Height, Color, Type) {
-    item.beginPath();
+function MakeObject(X, Y, Width, Height, Color, Type) { // Makes objects
+    canvas.beginPath();
     if (Type == "rect") {
-        item.rect(X, Y, Width, Height);
+        canvas.rect(X, Y, Width, Height);
     }
     else if (Type == "arc") {
-        item.arc(X + Width / 2, Y + Width / 2, Width / 2, 0, 2 * Math.PI);
-        item.fill();
+        canvas.arc(X + Width / 2, Y + Width / 2, Width / 2, 0, 2 * Math.PI);
     };
 
-    item.fillStyle = Color;
-    item.fill();
-}
-
-//
-function CheckObstacles(obstacle) {
-    for (let obs = 0; obs < obstacles.length; obs++) {
-        if (obstacle["x"] == (obstacles[obs]["x"]) + obstacles[obs]["width"] * 2) {
-            return false;
-        };
-    };
-    return true;
+    canvas.fillStyle = Color;
+    canvas.fill();
+    
 };
 
-//Update Game
-function Update() {
-    item.clearRect(0, 0, Canvas_Width, Canvas_Height);
-
-    /*if (GetRandom(0, 2) == 0) {
-        let randomObstacle = obstacles[GetRandom(0, obstacles.length)];
-        randomObstacle["y"] = Math.round((Math.random * Canvas_Height) / 20);
-        if (CheckObstacles(randomObstacle)) {
-            currentObstacles.pop(randomObstacle)
+function CheckObstacles(obstacle){ //Checks if obstacle is far enough away from other obstacles
+    let output = true;
+    for (let obs = 0; obs < currentObstacles.length; obs++) {
+        if ((obstacle["x"] <= currentObstacles[obs]["x"] + currentObstacles[obs]["width"] * 2)) {
+            output = false ;         
         };
     };
-
-    for (let numObstacles = 0; numObstacles < currentObstacles.length; numObstacles++) {
-        MakeItem(currentObstacles[numObstacles]["x"],currentObstacles[numObstacles]["y"],currentObstacles[numObstacles]["width"],currentObstacles[numObstacles]["height"],currentObstacles[numObstacles]["color"],currentObstacles[numObstacles]["type"],)
-    };*/
-
-    for (let o = 1; o < Player_Segments.length; o++) {
-        MakeItem(Player_Segments[o]["X"], Player_Segments[o]["Y"], Player_Size, Player_Size, Player_Color, "rect");
-    };
+    return output;
 };
-//Start game
+
+function Collision(segment){
+    console.log("collison")
+    for(let possibleCollison = 0; possibleCollison <= currentObstacles - 1; possibleCollison++)
+    {
+        
+        if(currentObstacles[possibleCollison]["x"] + currentObstacles.possibleCollison["width"] >= segment[possibleCollison]["x"] && currentObstacles[possibleCollison]["x"] <= segment[possibleCollison]["x"] + playerSize && currentObstacles[possibleCollison]["y"] + currentObstacles.possibleCollison["height"] >= segment[possibleCollison]["y"] && currentObstacles[possibleCollison]["y"] <= segment[possibleCollison]["y"] + playerSize ){
+            return true;      
+        };
+    };
+    return false;
+};
+
+function Update(){ //Creates objects shown on the canvas
+    canvas.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); //Resets canvas
+
+    for (let o = 0; o <= playerSegments.length - 1; o++) { //Creates all the segments for the player
+        console.log(Collision())
+        if(Collision(playerSegments[o])){
+            console.log("hello")
+        }
+        MakeObject(playerSegments[o]["x"], playerSegments[o]["y"], playerSize, playerSize, playerColor, "arc");
+    };
+
+    if (GetRandom(1, barrierChance + 1) == 1) { //Add barrier to currentObstacles
+        let barrier = { "name": "barrier", "type": "rect", "width": CANVAS_HEIGHT / 20, "height": CANVAS_HEIGHT / 20, "x": CANVAS_WIDTH, "y": 0, "color":"gray" }; //Standard Barrier
+        barrier["y"] = barrier["height"] * Math.round((GetRandom(0,CANVAS_HEIGHT - barrier["height"] + 1) ) / barrier["height"]); //Set random y of barrier
+        if (CheckObstacles(barrier)) { //If new barrier is far enough away from other barriers on the x
+            currentObstacles.push(JSON.parse(JSON.stringify(barrier))); //Add to currentObstacles 
+        };
+    };
+    
+    for (let numObstacles = 0; numObstacles < currentObstacles.length; numObstacles++) {//Make obstacles on canvas
+        currentObstacles[numObstacles]["x"] -= 1; //Moves the obstacles
+        if(currentObstacles[numObstacles]["x"] + currentObstacles[numObstacles]["width"] < 0){ //If an obstacle is past the view
+          currentObstacles.shift(); //Remove the obstacle
+        }
+        MakeObject(currentObstacles[numObstacles]["x"],currentObstacles[numObstacles]["y"],currentObstacles[numObstacles]["width"],currentObstacles[numObstacles]["height"],currentObstacles[numObstacles]["color"],currentObstacles[numObstacles]["type"]) //Create obstacles
+    };
+
+    
+};
+
 function Start() {
     timer = setInterval(Update, 1);
-    Playing = true;
+    playing = true;
 };
 
 function follow_segment() {
-    Player_Segments[0]["Y"] = Player_Y;
+    playerSegments[0]["y"] = playerY;
     let Current_Segment = 1;
     let follow = setInterval(function () {
-        if (Current_Segment >= Player_Segments.length) {
+        if (Current_Segment >= playerSegments.length) {
             clearInterval(follow);
         }
         else {
             // set position of curent segment to previous segmentin thelist
             // each segment follow the segment in front of it.
-            Player_Segments[Current_Segment]["Y"] = Player_Segments[Current_Segment - 1]["Y"];
+            playerSegments[Current_Segment]["y"] = playerSegments[Current_Segment - 1]["y"];
             Current_Segment++;
         };
     }, 5);
 };
-
 //keys
 document.addEventListener("keypress", function (event) {
-    if (event.key == "Enter" && Playing == false) {
+    if (event.key == "Enter" && playing == false) {
         Start();
     }
     else if (event.key == "w" || event.key == "s") {
-        // Adding key to list if it does not exist
-        if (!Pressed_Keys.includes(event.key)) {
-            Pressed_Keys.push(event.key);
+        if (!pressedKeys.includes(event.key)) {
+            pressedKeys.push(event.key);
         };
     };
 });
 
 // Removing key you lifted up from list
 document.addEventListener("keyup", function (event) {
-    Pressed_Keys.splice(Pressed_Keys.indexOf(event.key), Pressed_Keys.indexOf(event.key) + 1);
+    pressedKeys.splice(pressedKeys.indexOf(event.key), pressedKeys.indexOf(event.key) + 1);
 });
 
-let followSpeed = 15;
-// Follow w & s keys
 let Keys_timer = setInterval(function () {
-    if (Pressed_Keys[0] == "w" && Player_Y >= 1) {
-        Player_Y -= 10;
+    if (pressedKeys[0] == "w" && playerY >= 1) {
+        playerY -= 1;
         follow_segment();
     }
-    else if (Pressed_Keys[0] == "s" && Player_Y <= Canvas_Height - Player_Size) {
-        Player_Y += 10;
+    else if (pressedKeys[0] == "s" && playerY <= CANVAS_HEIGHT - playerSize) {
+        playerY += 1;
         follow_segment();
     };
 
-}, followSpeed);
+}, 15);
