391da576db4afcb09fbe45cc001ee7ae9cddeaa4
echoaj <aljoslin13@yahoo.com>
2022-06-21 16:29:33 -0700

    ethan lesson

---
+++ Practice_JS/7_quiz/app.js
@@ -0,0 +1,32 @@
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Step 1
+/*app.get('/', (req, res) => { 
+    res.send('<h1>Hello World</h1>');
+});*/
+
+// Step 2
+/*app.get('/', (req, res) => { 
+    res.sendFile(__dirname + '/frontend/index.html');
+});*/
+
+// Step 3
+app.use(express.static('frontend'));
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('<h1>Form Submitted!</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
+
+
