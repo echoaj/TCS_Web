a79d8206eb1d4fe1ab0ac2a5f08d714d17167cd2
echoaj <aljoslin13@yahoo.com>
2022-06-28 16:27:25 -0700

    Ethan lesson complete

--- Jake_&_Ethan/Practice_JS/7_quiz/app.js
+++ Jake_&_Ethan/Practice_JS/7_quiz/app.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Middlewhere
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.static('frontend'));
+
+// Function executes when user creates a post request form the browser
+app.post('/', (req, res) => {
+    res.send('<h1>Thank you for your submission!</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+});
