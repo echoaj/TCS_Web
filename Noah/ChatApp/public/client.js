2738cf2fc7cb40af1c61ee36107a6f75cd778372
echoaj <aljoslin13@yahoo.com>
2022-08-25 18:32:30 -0700

    Noah lesson

--- Noah/ChatApp/public/client.js
+++ Noah/ChatApp/public/client.js
@@ -1,11 +1,26 @@
+const socket = io('http://localhost:3000');
 const msgInput = document.getElementById('msg_input');
 const btnMsg = document.getElementById('btn_msg');
 const chat = document.getElementById('chat');
 
+const btnUsr = document.getElementById('btn_user');
+const usrInput = document.getElementById('username');
+
 
 btnMsg.addEventListener('click', () => {
     const msg = msgInput.value;
+    socket.emit('chat message', msg);
+});
+
+btnUsr.addEventListener('click', () => {
+    const usr = usrInput.value;
+    socket.emit('username', usr);
+});
+
+
+socket.on('chat message', (data) => {
+    console.log('Received message from server: ' + data.message);
     const h2 = document.createElement('h2');
-    h2.innerText = msg;
-    chat.appendChild(h2)
+    h2.innerText = data.username + ": " + data.message;
+    chat.appendChild(h2);
 });
--- Noah/ChatApp/server.js
+++ Noah/ChatApp/server.js
@@ -1,2 +1,35 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const PORT = 3000;
 
-console.log("Starting server...");
+
+// Middleware
+app.use(express.static('public'))
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
+
+
+io.on('connection', (socket) => {
+    console.log('New user connected', socket.id);
+
+    socket.username = "Anonymous";
+
+    socket.on('chat message', (message) => {
+        console.log("Received message from client: " + message);
+        // Send message to all clients
+        io.sockets.emit('chat message', {"message": message, "username": socket.username});
+        // Send message to all clients except the sender
+        // send mesage to only the sender
+        // socket.emit('chat message', message);
+    })
+
+
+    socket.on('username', (username) => {
+        socket.username = username;
+    });
+
+})
